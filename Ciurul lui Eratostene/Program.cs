2aabe55db012ae47c39b2686ca431abd2b896936
Andrei Kovacs <andreijok678@yahoo.com>
2020-11-03 13:30:55 +0200

    Added Ciurul lui Eratostene

---
+++ Ciurul lui Eratostene/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Ciurul_lui_Eratostene
+{
+    class Program
+    {
+        static void ciur(int[] x, int n)
+        {
+            for (int i = 2; i <= n; i++)
+                if (x[i] == 0)
+                {
+                    Console.Write($"{i} ");
+                    for (int j = i + i; j <= n; j += i)
+                        x[j] = 1;
+                }
+        }
+        static void Main(string[] args)
+        {
+            Console.Write(" Problema: Să se genereze toate numerele prime mai mici decât numărul natural n dat.\n\n");
+            Console.Write(" Introduceti un numar n\n ");
+
+            int n = int.Parse(Console.ReadLine());
+            int[] x = new int[n + 1];
+
+            Console.Write($"\n Toate numerele prime mai mici decat {n} sunt:\n ");
+            ciur(x, n);
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.Write(" Animatie reprezentativa: https://upload.wikimedia.org/wikipedia/commons/b/b9/Sieve_of_Eratosthenes_animation.gif \n ");
+        }
+    }
+}
